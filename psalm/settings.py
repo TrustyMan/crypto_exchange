0d475b4f9b7b33628e449ed2ad66938336206554
Taurus Luo <luo.james2017@yandex.com>
2018-02-02 17:01:01 +0530

    Update settings.py

--- psalm/settings.py
+++ psalm/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '-r-js1vhi*br(*u+@&@64=io0rtn2n@xo%ir5xc37hqztpe%*9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
